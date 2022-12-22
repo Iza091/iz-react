ac73d792a0bc73b8ffeffb574089879d12c68d3d
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-12-22 16:59:02 -0600

    React, entendiendo su uso

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/NavBar.js';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editando react <code>'src/App.js'</code> y aprendiendo como se usa.
         </p>
         <a
           className="App-link"
@@ -20,6 +21,7 @@ function App() {
       </header>
     </div>
   );
+
 }
 
 export default App;
