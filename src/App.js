b2dfdac454e05af656832fa41362254003756353
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-12-24 19:54:14 -0600

    Jugando con Js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Card from  './components/Card'
+import Clock from './components/Clock';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-center"> Kim Dahyun</h1>
         <Card/>
+        <span id="res"></span>
+        <Clock/>
       </header>
+      
+      
     </div>
   );
 
